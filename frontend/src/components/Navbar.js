640d6a6e6d35408a9b4e7d0e8405aaffe01f7bd4
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-02 23:39:15 +0800

    Remove navbar elevation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" elevation={0}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "white" }}>
             PeerPrep
