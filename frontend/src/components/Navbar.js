a568e18a58c51b6083f46e4280592b5284128e91
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-02 23:13:33 +0800

    Add navbar component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            PeerPrep
+          </Typography>
+          <Button color="inherit">Signin</Button>
+          <Button color="inherit">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
