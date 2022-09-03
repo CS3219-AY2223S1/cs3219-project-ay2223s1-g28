7f9b1fb32e348cc4f6f0464535aa33527e0dbd6a
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-03 17:24:41 +0800

    Update signin form styling

--- frontend/src/components/forms/SigninForm.js
+++ frontend/src/components/forms/SigninForm.js
@@ -5,38 +5,42 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 import IconTextField from '../ui/IconTextField';
+import styles from './SigninForm.module.css';
 
 function SigninForm() {
   return (
     <Grid
+      className={styles.signin_form}
       container
-      direction="column"
       justifyContent="center"
       alignItems="center"
       spacing={3}
     >
-      <Grid item>
+      <Grid item xs={12}>
         <IconTextField
           icon={<AccountCircle color="primary" fontSize="large" />}
           label="Email/Username"
         />
       </Grid>
-      <Grid item>
+      <Grid item xs={12}>
         <IconTextField
           icon={<HttpsOutlinedIcon color="primary" fontSize="large" />}
           label="Password"
         />
       </Grid>
-      <Grid item>
-        <Button variant="outlined" size="large">
+      <Grid
+        item
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Button className={styles.signin_button} variant="outlined" size="large">
           Sign in
         </Button>
-      </Grid>
-      <Grid item>
         <Typography variant="button">
-          Don't have an account? 
+          Don't have an account? <Button href="/signup">Sign up here!</Button>
         </Typography>
-        <Button href="/signup">Sign up here!</Button>
       </Grid>
     </Grid>
   );
