7880b45e058b6b6c2f26e54f311371a71d5d123f
hsiaojietng <e0556311@u.nus.edu>
2022-09-04 12:03:02 +0800

    Create question difficulty page/home page

--- frontend/src/components/ui/DifficultyButtonGroup.js
+++ frontend/src/components/ui/DifficultyButtonGroup.js
@@ -1,31 +1,25 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup, Container } from "@mui/material";
+
+import React from "react";
 
 import styles from "./DifficultyButtonGroup.module.css";
 
 function DifficultyButtonGroup(props) {
   return (
     <>
-      <Button
-        className={styles.easy}
-        variant="outlined"
-        size="large"
-      >
-        Easy
-      </Button>
-      <Button
-        className={styles.medium}
-        variant="outlined"
-        size="large"
-      >
-        Medium
-      </Button>
-      <Button
-        className={styles.hard}
-        variant="outlined"
-        size="large"
-      >
-        Hard
-      </Button>
+      <Container className={styles.container}>
+        <ButtonGroup>
+          <Button className={styles.easy} variant="outlined" size="large">
+            Easy
+          </Button>
+          <Button className={styles.medium} variant="outlined" size="large">
+            Medium
+          </Button>
+          <Button className={styles.hard} variant="outlined" size="large">
+            Hard
+          </Button>
+        </ButtonGroup>
+      </Container>
     </>
   );
 }
--- frontend/src/pages/DifficultySelection/index.js
+++ frontend/src/pages/DifficultySelection/index.js
@@ -7,40 +7,90 @@ import DifficultyButtonGroup from "../../components/ui/DifficultyButtonGroup";
 function DifficultySelectionPage() {
   return (
     <>
-      <img
-        src={require("../../assets/difficultyselectionpage_media.svg").default}
-        alt="Code"
-        width="100%"
-      />
-      <OutlinedContainer>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <Grid item xs={5}>
+      <Grid container>
+        <Grid container direction="column" item xs={3} alignItems="center">
+          <Grid item>
+            <Typography
+              variant="h2"
+              fontWeight="lighter"
+              textAlign="center"
+              sx={{ mt: "32px" }}
+              color="#3EA7A5"
+            >
+              Welcome to PeerPrep
+            </Typography>
+          </Grid>
+          <Grid item>
             <Typography
               variant="h5"
               fontWeight="lighter"
               textAlign="center"
               sx={{ mt: "32px" }}
+              color="#A7C7C8"
             >
-              Question Difficulty
+              Your one stop <br />
+              solution to acing your <br />
+              technical interviews
             </Typography>
-            <Typography
-              variant="h5"
-              fontWeight="lighter"
-              textAlign="center"
-              sx={{ mt: "32px" }}
-            >
-              Start by choosing a difficulty level for your coding question. Let
-              PeerPrep handle the rest!
-            </Typography>
-            <DifficultyButtonGroup />
+          </Grid>
+          <Grid item>
+            <img
+              src={
+                require("../../assets/difficultyselectionpage_media.svg")
+                  .default
+              }
+              alt="Code"
+              height="90%"
+              width="90%"
+            />
           </Grid>
         </Grid>
-      </OutlinedContainer>
+        <Grid item xs={8} container>
+          <OutlinedContainer>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography
+                  variant="h3"
+                  fontWeight="lighter"
+                  textAlign="center"
+                  sx={{ mt: "32px" }}
+                  color="#3EA7A5"
+                >
+                  Question Difficulty
+                </Typography>
+                <Typography
+                  variant="h5"
+                  fontWeight="lighter"
+                  textAlign="center"
+                  sx={{ mt: "100px" }}
+                  color="#A7C7C8"
+                >
+                  Start by choosing <br />a{" "}
+                  <Typography variant="h4" component="span" fontWeight="bold">
+                    difficulty
+                  </Typography> level for <br />
+                  your coding question.
+                  <br />
+                  Let PeerPrep handle the rest!
+                </Typography>
+                <Grid
+                  item
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{ mt: "120px" }}
+                >
+                  <DifficultyButtonGroup />
+                </Grid>
+              </Grid>
+            </Grid>
+          </OutlinedContainer>
+        </Grid>
+      </Grid>
     </>
   );
 }
