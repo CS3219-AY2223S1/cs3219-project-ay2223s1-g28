d62e214e32e264ae35ab815caf326b7f9c0dd584
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-20 01:36:16 +0800

    Implement navbar update on auth status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { useContext, useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import AppBar from '@mui/material/AppBar';
@@ -5,8 +7,23 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
+import UserContext from '../context/user-context';
 
 export default function ButtonAppBar() {
+  let userCtx = useContext(UserContext);
+  let isSignedIn = userCtx.isSignedIn;
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => setAnchorEl(event.currentTarget);
+
+  const handleClose = () => setAnchorEl(null);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" elevation={0}>
@@ -14,8 +31,32 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "white" }}>
             PeerPrep
           </Typography>
-          <Button color="secondary" size="large" component={Link} to="/signin">Sign in</Button>
-          <Button color="secondary" size="large" component={Link} to="signup">Sign up</Button>
+          {!isSignedIn && <Button color="secondary" size="large" component={Link} to="/signin">Sign in</Button>}
+          {!isSignedIn && <Button color="secondary" size="large" component={Link} to="signup">Sign up</Button>}
+          {isSignedIn && (
+            <div>
+              <IconButton size="large" onClick={handleMenu} color="secondary">
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
