6921d601e9333bb9c263bef6da7305ebb43ef461
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-20 12:18:58 +0800

    Add backdrop warning for deleting account

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -7,15 +7,21 @@ import Grid from '@mui/material/Grid';
 import WarningIcon from '@mui/icons-material/Warning';
 
 import UpdateProfileForm from '../../components/forms/UpdateProfileForm';
+import CustomBackdrop from '../../components/ui/CustomBackdrop';
 
 import AlertContext from '../../context/alert-context';
 import UserContext from '../../context/user-context';
+import { Typography } from '@mui/material';
 
 function ProfilePage() {
   const alertCtx = useContext(AlertContext);
   const userCtx = useContext(UserContext);
 
+  const [openBackdrop, setOpenBackdrop] = useState(false);
+
   const navigate = useNavigate();
+
+  const toggleBackdropHandler = () => setOpenBackdrop(!openBackdrop);
 
   const deleteAccountHandler = () => {
     userCtx.onDeleteAccount((deleteSuccessMessage, err) => {
@@ -37,7 +43,7 @@ function ProfilePage() {
           size="large"
           color="error"
           startIcon={<WarningIcon />}
-          onClick={deleteAccountHandler}
+          onClick={toggleBackdropHandler}
         >
           Delete Account
         </Button>
@@ -45,6 +51,18 @@ function ProfilePage() {
       <Grid item>
         <UpdateProfileForm />
       </Grid>
+      <CustomBackdrop
+        openBackdrop={openBackdrop}
+        onClick={toggleBackdropHandler}
+        content={
+          <Typography>Are you sure you want to delete this account?</Typography>
+        }
+        primaryAction={{ text: 'Delete', onClick: deleteAccountHandler }}
+        secondaryAction={{
+          text: 'No',
+          onClick: toggleBackdropHandler,
+        }}
+      />
     </Grid>
   );
 }
