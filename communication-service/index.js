69b2ff20df83030a1f72e44b8d5f1707006df485
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-11 14:51:45 +0800

    Set up communication server

---
+++ communication-service/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World from communication-service');
+});
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { 
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  }); 
+  
+httpServer.listen(8080, () => console.log('communication-service listening on port 8080'));
