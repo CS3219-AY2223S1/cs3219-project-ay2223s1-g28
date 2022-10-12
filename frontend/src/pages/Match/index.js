e1b6fc11668ab4337d1a82cf0ccc04f44fe435ba
xyliew25 <e0550381@u.nus.edu>
2022-10-13 01:15:42 +0800

    Add matching frontend logic

--- frontend/src/pages/Match/index.js
+++ frontend/src/pages/Match/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
@@ -8,8 +8,10 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
+import io from 'socket.io-client';
+
+import { URL_MATCHING_SVC } from '../../configs';
 import OutlinedContainer from '../../components/ui/OutlinedContainer';
-import Timer from './Timer';
 import styles from './Match.module.css';
 
 // Styling
@@ -19,24 +21,82 @@ const PADDED_BTN = {
   paddingLeft: 6,
   paddingRight: 6,
 }
+// Timer
+const DURATION = 30; 
 
 function MatchPage() {
   // Hooks
-  const [isMatchFailed, setIsMatchFailed] = useState(false);
-  const navigate = useNavigate();
   const location = useLocation();
+  const navigate = useNavigate();
+  // Difficulty
+  const [difficulty, setDifficulty] = useState('');
+  // Socket
+  const [socket, setSocket] = useState(null);
+  const [room, setRoom] = useState('');
+  const [isMatchFailed, setIsMatchFailed] = useState(false);
+  // Timer
+  const [counter, setCounter] = useState(DURATION);
+  const [timerEnd, setTimerEnd] = useState(false);
+
+  // useEffect
+  // Difficulty
+  useEffect(() => {
+    if (location.state) {
+      setDifficulty(location.state?.difficulty);
+    }
+  }, [location.state]);
+  // Socket
+  useEffect(() => {
+    setSocket(io.connect(URL_MATCHING_SVC));
+  }, [])
+  useEffect(() => {
+    if (socket) {
+      socket.on('matchSuccess', (room) => {
+        setRoom(room);
+      });
+      socket.on('matchFail', () => {
+        setIsMatchFailed(true);
+      });
+      socket.on('disconnect', () => {
+        setIsMatchFailed(true);
+      });
+      return () => socket.disconnect();
+    }
+  }, [socket]);
+  useEffect(() => {
+    if (difficulty && socket) {
+      socket.emit('match', { difficulty });
+    }
+  }, [difficulty, socket])
+  useEffect(() => {
+    if (room && navigate) {
+      room && navigate('/room', { state: { room } });
+    }
+  }, [room, navigate]);
+  // Timer
+  useEffect(() => {
+    if (counter > 0) {
+      setTimeout(() => setCounter(prevCounter => prevCounter - 1), 1000);
+    } else {
+      setTimerEnd(true);
+    }
+  }, [counter]);
 
   // Handlers
   const handleCancel = () => {
+    socket.emit('cancelMatch', { id: socket.id });
     navigate('/home');
   };
   const handleRetry = () => {
     setIsMatchFailed(false);
+    setTimerEnd(false);
+    setCounter(DURATION);
+    socket.emit('match', { difficulty });
   }
 
   // Styling
   let boxStyling = styles.difficultyBox;
-  switch (location.state?.difficulty) {
+  switch (difficulty) {
     case 'Easy':
       boxStyling += ' ' + styles.easy;
       break;
@@ -55,13 +115,13 @@ function MatchPage() {
       <Grid container direction='column' alignItems='center' padding={3}>
         <Grid item>
           <div className={boxStyling}>
-            <Typography variant='h5' color='white'>{location.state?.difficulty}</Typography>
+            <Typography variant='h5' color='white'>{difficulty}</Typography>
           </div>
         </Grid>
 
         <Grid item mt={2} className={styles.spaceAround}>
           <PersonOutlineIcon color='primary' sx={{ fontSize: 80 }} />
-          {isMatchFailed
+          {isMatchFailed && timerEnd
             ? <CancelOutlinedIcon color='error' sx={{ fontSize: 55}} />
             : <CircularProgress sx={{ color: DARKGREY, margin: 1 }} />}
           <PersonOutlineIcon color='primary' sx={{ fontSize: 80 }} />
@@ -69,7 +129,7 @@ function MatchPage() {
 
         <Grid item mt={2}>
           <Typography variant='h6' textAlign='center' sx={{ color: GREY, lineHeight: 1.3 }}>
-            {isMatchFailed
+            {isMatchFailed && timerEnd
             ? <>Looks like no one is willing to<br />
                 take on your challenge yet.<br />
                 Let's try matching again!</>
@@ -79,16 +139,24 @@ function MatchPage() {
         </Grid>
 
         <Grid item mt={5}>
-          <Typography variant='h6' display='inline' sx={{ color: DARKGREY }}>
-            Time left:{' '}
-          </Typography>
-          <Typography variant='h5' display='inline' sx={{ color: DARKGREY }}>
-            <Timer /> sec
-          </Typography>
+          {!isMatchFailed && timerEnd
+            ? <Typography variant='h6' display='inline' sx={{ color: DARKGREY }}>
+                Loading...
+              </Typography>
+            : isMatchFailed && timerEnd
+            ? <></>
+            : <>
+                <Typography variant='h6' display='inline' sx={{ color: DARKGREY }}>
+                  Time left:{' '}
+                </Typography>
+                <Typography variant='h5' display='inline' sx={{ color: DARKGREY }}>
+                  {counter} sec
+                </Typography>
+              </>}
         </Grid>
 
         <Grid item mt={10} className={styles.spaceAround}>
-          {isMatchFailed
+          {isMatchFailed && timerEnd
           ? <>
               <Button onClick={handleRetry} variant='outlined' size='large' sx={{ color: '#a7c7c8', ...PADDED_BTN }}>
                 Retry
