649287bb2a7b3bbfc7db617c813a16f44aac85f0
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-12 09:55:11 +0800

    Complete communication service implementation

--- communication-service/index.js
+++ communication-service/index.js
@@ -3,7 +3,6 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
-
 app.get('/', (req, res) => {
   res.send('Hello World from communication-service');
 });
@@ -11,13 +10,24 @@ app.get('/', (req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: ['http://localhost:3000'],
   },
 });
 
 io.on('connection', (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on('send-chat', (roomId, senderUsername, chatMessage) => {
+    if (roomId !== '') {
+      /*
+        When one client send a chat message, every other client(s) 
+        with the same roomId will receive the chat.
+      */
+      socket.to(roomId).emit('receive-chat', senderUsername, chatMessage);
+    }
+  });
 });
 
 httpServer.listen(8080, () =>
