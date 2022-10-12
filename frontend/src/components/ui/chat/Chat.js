e128bb55f36c3c3f4c0848ac65503d0e8f85a07c
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-12 09:57:37 +0800

    Render sender username instead of id

--- frontend/src/components/ui/chat/Chat.js
+++ frontend/src/components/ui/chat/Chat.js
@@ -1,10 +1,10 @@
-import styles from "./Chat.module.css";
+import styles from './Chat.module.css';
 
 function Chat(props) {
   return (
     <div className={styles.chat_container}>
       <div className={styles.chat_sender}>
-        <h4>{props.senderId}: </h4>
+        <h4>{props.senderUsername}: </h4>
       </div>
       <div className={styles.chat_text}>{props.text}</div>
     </div>
