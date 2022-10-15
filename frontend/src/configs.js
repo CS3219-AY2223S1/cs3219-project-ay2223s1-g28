d9ab5b60e50b7825c9645a33d05aba2410b8c639
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-16 01:57:33 +0800

    Add communication service URL to config

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,5 +1,7 @@
 const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
+const URI_COMM_SVC = process.env.URI_COMM_SVC || 'http://localhost:8002'
 
 const PREFIX_USER_SVC = '/api/user'
 
 export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC
+export const URL_COMM_SVC = URI_COMM_SVC // Add PREFIX in the future if needed
