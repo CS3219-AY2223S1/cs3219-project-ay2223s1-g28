a25bbac3e485b270fe2230605f4b29a4cc94b856
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-18 22:06:12 +0800

    Update chat document schema

--- communication-service/model/chat-model.js
+++ communication-service/model/chat-model.js
@@ -1,26 +1,26 @@
 import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 let ChatModelSchema = new Schema({
-    rid: {
-        type: ObjectId,
-        required: true,
-    },
-    index: {
-        type: Number,
-        required: true,
-    },
-    sender: {
-        type: ObjectId,
-        required: true,
-    },
-    receiver: {
-        type: ObjectId,
-        required: true,
-    },
-    text: {
-        type: String,
-        required: true,
-    },
+	rid: {
+		type: String,
+		required: true,
+	},
+	index: {
+		type: Number,
+		required: true,
+	},
+	sender: {
+		type: String,
+		required: true,
+	},
+	receiver: {
+		type: String,
+		required: true,
+	},
+	text: {
+		type: String,
+		required: true,
+	},
 });
 
 ChatModelSchema.index({ rid: 1, index: 1 }, { unique: true });
