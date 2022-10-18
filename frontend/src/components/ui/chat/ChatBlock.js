ed6144a8c7fc6f0863811b6a52658ead2884df17
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-19 00:21:55 +0800

    Format indentation by tabs

--- frontend/src/components/ui/chat/ChatBlock.js
+++ frontend/src/components/ui/chat/ChatBlock.js
@@ -21,20 +21,20 @@ import { URL_COMM_SVC_CREATE_CHAT } from '../../../configs';
 const ENTER_KEY_CODE = 13;
 
 function ChatBlock({ socket, roomId }) {
-  const alertCtx = useContext(AlertContext);
+	const alertCtx = useContext(AlertContext);
 	const userCtx = useContext(UserContext);
 	const [chats, setChats] = useState([]);
 
-  const {
+	const {
 		value: chatMessage,
 		isValid: chatIsValid,
 		valueChangeHandler: chatChangeHandler,
 		reset: resetChat,
 	} = useInput(isValidChat);
 
-  const myUsername = userCtx.username;
+	const myUsername = userCtx.username;
 
-  useEffect(() => {
+	useEffect(() => {
 		// Using useeffect hook to avoid rendering duplicate copies of chat received
 		// (Only render chat block component once after receive message)
 		socket.on('receive-chat', (senderUsername, chatMessage) => {
@@ -42,16 +42,17 @@ function ChatBlock({ socket, roomId }) {
 			axios
 				.post(URL_COMM_SVC_CREATE_CHAT, {
 					roomId,
-					chatIndex: chats.length,
 					sender: senderUsername,
 					receiver: myUsername,
 					text: chatMessage,
 				})
 				.then(
-          // Update user UI only after the chat is added to database
+					// Update user UI only after the chat is added to database
 					setChats((chats) => [...chats, { senderUsername, text: chatMessage }])
 				)
-				.catch((err) => alertCtx.onShow(err.response.data.message));
+				.catch((err) => {
+					alertCtx.onShow(err.response.data.message);
+				});
 		});
 
 		// The listeners must be removed in the cleanup step,
@@ -59,53 +60,53 @@ function ChatBlock({ socket, roomId }) {
 		return () => socket.off('receive-chat');
 	}, [socket, roomId, alertCtx, myUsername, chats.length]);
 
-  const sendChatHandler = () => {
-    if (!chatIsValid) {
-      resetChat();
-      return;
-    }
+	const sendChatHandler = () => {
+		if (!chatIsValid) {
+			resetChat();
+			return;
+		}
 
-    socket.emit('send-chat', roomId, myUsername, chatMessage);
+		socket.emit('send-chat', roomId, myUsername, chatMessage);
 
-    setChats((chats) => [
-      ...chats,
-      { senderUsername: 'Me', text: chatMessage },
-    ]);
+		setChats((chats) => [
+			...chats,
+			{ senderUsername: 'Me', text: chatMessage },
+		]);
 
-    resetChat();
-  };
+		resetChat();
+	};
 
-  const onKeyDownHandler = (event) => {
-    // Send the message only if the user press ENTER
-    if (event.keyCode === ENTER_KEY_CODE) {
-      event.preventDefault();
-      sendChatHandler();
-    }
-  }
+	const onKeyDownHandler = (event) => {
+		// Send the message only if the user press ENTER
+		if (event.keyCode === ENTER_KEY_CODE) {
+			event.preventDefault();
+			sendChatHandler();
+		}
+	};
 
-  return (
-    <div className={styles.chat_block}>
-      <Title />
-      <Divider orientation="horizontal" />
-      <ChatList chats={chats} />
-      <OutlinedInput
-        placeholder="Chat here..."
-        className={styles.chat_input}
-        value={chatMessage}
-        onChange={chatChangeHandler}
-        onKeyDown={onKeyDownHandler}
-        endAdornment={
-          chatMessage && (
-            <InputAdornment position="end">
-              <IconButton onClick={sendChatHandler} edge="end">
-                <SendIcon color="primary" />
-              </IconButton>
-            </InputAdornment>
-          )
-        }
-      ></OutlinedInput>
-    </div>
-  );
+	return (
+		<div className={styles.chat_block}>
+			<Title />
+			<Divider orientation="horizontal" />
+			<ChatList chats={chats} />
+			<OutlinedInput
+				placeholder="Chat here..."
+				className={styles.chat_input}
+				value={chatMessage}
+				onChange={chatChangeHandler}
+				onKeyDown={onKeyDownHandler}
+				endAdornment={
+					chatMessage && (
+						<InputAdornment position="end">
+							<IconButton onClick={sendChatHandler} edge="end">
+								<SendIcon color="primary" />
+							</IconButton>
+						</InputAdornment>
+					)
+				}
+			></OutlinedInput>
+		</div>
+	);
 }
 
 export default ChatBlock;
