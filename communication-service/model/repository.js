e809d7def1dfb79780793010c756fb369fd9b210
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-10-18 22:34:11 +0800

    Use const variable

--- communication-service/model/repository.js
+++ communication-service/model/repository.js
@@ -5,14 +5,14 @@ import ChatModel from './chat-model.js';
 //Set up mongoose connection
 import mongoose from 'mongoose';
 
-let mongoDB =
+const mongoDB =
 	process.env.ENV == 'PROD'
 		? process.env.DB_CLOUD_URI
 		: process.env.DB_LOCAL_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 export async function createChat(params) {
