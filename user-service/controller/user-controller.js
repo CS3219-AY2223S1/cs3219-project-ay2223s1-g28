d9fc03213b57a49c165cab57daaaf57842abd870
Pua Li Xue <77202052+pualixue@users.noreply.github.com>
2022-10-20 13:23:47 +0800

    Update user-service/controller/user-controller.js
    Co-authored-by: Liew Xin Yi <75534016+xyliew25@users.noreply.github.com>

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -25,7 +25,8 @@ export async function createUser(req, res) {
                 return res.status(201).json({message: `Created new user ${username} successfully!`});
             }
         } else {
-            return res.status(400).json({message: 'Username/Email/Password are missing!'});
+            return res.status(400).json({ message: 'Username/Email/Password is/are missing!' });
+
         }
     } catch (err) {
         return res.status(500).json({message: 'Database failure when creating new user!'})
