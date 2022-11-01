f710eff24698985cb49c5b315aa9384190e3fd5f
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-11-01 18:32:57 +0800

    Arrange buttons responsively and update margin

--- frontend/src/components/ui/DifficultyButtonGroup.js
+++ frontend/src/components/ui/DifficultyButtonGroup.js
@@ -1,16 +1,19 @@
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Container from "@mui/material/Container";
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Container from '@mui/material/Container';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-import DifficultyButton from "./DifficultyButton.js";
-import styles from "./DifficultyButtonGroup.module.css";
+import DifficultyButton from './DifficultyButton.js';
+import styles from './DifficultyButtonGroup.module.css';
 
 function DifficultyButtonGroup() {
+  const isMobileView = useMediaQuery('(max-width:900px)');
+
   return (
     <Container className={styles.container}>
-      <ButtonGroup>
+      <ButtonGroup orientation={isMobileView ? 'vertical' : 'horizontal'}>
         <DifficultyButton buttonText="Easy" id="easyDifficulty" />
-        <DifficultyButton buttonText="Medium" id="mediumDifficulty"/>
-        <DifficultyButton buttonText="Hard" id="hardDifficulty"/>
+        <DifficultyButton buttonText="Medium" id="mediumDifficulty" />
+        <DifficultyButton buttonText="Hard" id="hardDifficulty" />
       </ButtonGroup>
     </Container>
   );
