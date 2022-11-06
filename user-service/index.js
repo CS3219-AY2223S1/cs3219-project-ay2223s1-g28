edcd0cbddf3e2e86404030dfa07731277a6bca32
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-11-07 07:50:57 +0800

    Add user service response header back

--- user-service/index.js
+++ user-service/index.js
@@ -34,4 +34,10 @@ router.post('/update', authenticateJwt, updateAccount);
 router.post('/delete', authenticateJwt, deleteAccount);
 router.get('/verify-jwt', authenticateJwt, acknowledgeJWTValidity);
 
+app.use('/api/user', router).all((_, res) => {
+  res.setHeader('content-type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', process.env.ENV == 'PROD'? process.env.FRONTEND_URL : 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+});
+
 app.listen(8000, () => console.log('user-service listening on port 8000'));
