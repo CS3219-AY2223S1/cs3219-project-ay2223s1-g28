f4604fc06a89736c7d3be33e51db9eb4f05ddc7e
xyliew25 <e0550381@u.nus.edu>
2022-11-06 23:01:20 +0800

    Add auth to matching service

--- matching-service/index.js
+++ matching-service/index.js
@@ -15,6 +15,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
+    credentials: true,
   },
   path: '/api/matching-service/socket',
 });
