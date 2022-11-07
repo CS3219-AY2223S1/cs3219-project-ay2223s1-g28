aa3d6e69d20942d1f5844adae8c1cad9384e1f5d
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-11-08 03:24:05 +0800

    Add signin prop

--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,47 +1,29 @@
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-import OutlinedContainer from '../../components/ui/OutlinedContainer';
 import SigninForm from '../../components/forms/SigninForm';
-import GreenDivider from '../../components/ui/GreenDivider';
 
 function SigninPage() {
   const isMobileView = useMediaQuery('(max-width:900px)');
 
   return (
-    <OutlinedContainer>
-      <Grid
-        container
-        direction={isMobileView ? 'column' : 'row'}
-        justifyContent="space-evenly"
-        alignItems="center"
-        gap={5}
-      >
-        <Grid item xs={5}>
-          <Typography
-            variant="h5"
-            fontWeight="lighter"
-            textAlign="center"
-            sx={{ mt: '32px' }}
-          >
-            Start acing your technical interview with{' '}
-            <Typography variant="h5" component="span" fontWeight="bold">
-              PeerPrep
-            </Typography>
-          </Typography>
-          <img
-            src={require('../../assets/signin_form_media.svg').default}
-            alt="Signin form"
-            width="100%"
-          />
-        </Grid>
-        <GreenDivider />
-        <Grid item xs={5}>
-          <SigninForm />
-        </Grid>
+    <Grid
+      container
+      direction={isMobileView ? 'column' : 'row'}
+      alignItems="center"
+    >
+      <Grid item xs={6}>
+        <img
+          src={require('../../assets/signin.svg').default}
+          alt="Signin"
+          height="100%"
+          width="100%"
+        />
       </Grid>
-    </OutlinedContainer>
+      <Grid item xs={6}>
+        <SigninForm />
+      </Grid>
+    </Grid>
   );
 }
 
