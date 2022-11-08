9c8d2bbce6fd8ced8d52b844544e9b1ed6d0f4d2
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-11-08 12:44:26 +0800

    Add background

--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,19 +1,22 @@
 import SplitterContainer from '../../components/layout/SplitterContainer';
 import SigninForm from '../../components/forms/SigninForm';
+import GradientBackground from '../../components/layout/GradientBackground';
 
 function SigninPage() {
   return (
-    <SplitterContainer
-      primaryChild={
-        <img
-          src={require('../../assets/signin.svg').default}
-          alt="Signin"
-          height="100%"
-          width="100%"
-        />
-      }
-      secondaryChild={<SigninForm />}
-    />
+    <GradientBackground>
+      <SplitterContainer
+        primaryChild={
+          <img
+            src={require('../../assets/signin.svg').default}
+            alt="Signin"
+            height="100%"
+            width="100%"
+          />
+        }
+        secondaryChild={<SigninForm />}
+      />
+    </GradientBackground>
   );
 }
 
--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -1,19 +1,22 @@
 import SplitterContainer from '../../components/layout/SplitterContainer';
 import SignupForm from '../../components/forms/SignupForm';
+import GradientBackground from '../../components/layout/GradientBackground';
 
 function SignupPage() {
   return (
-    <SplitterContainer
-      primaryChild={
-        <img
-          src={require('../../assets/signup.svg').default}
-          alt="Signup"
-          width="100%"
-          height="100%"
-        />
-      }
-      secondaryChild={<SignupForm />}
-    />
+    <GradientBackground>
+      <SplitterContainer
+        primaryChild={
+          <img
+            src={require('../../assets/signup.svg').default}
+            alt="Signup"
+            width="100%"
+            height="100%"
+          />
+        }
+        secondaryChild={<SignupForm />}
+      />
+    </GradientBackground>
   );
 }
 
