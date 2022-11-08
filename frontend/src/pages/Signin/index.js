28fc6aeda2149fef3219c7cdc05e274471177c8b
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-11-08 12:22:14 +0800

    Style signin and signup page

--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,29 +1,19 @@
-import Grid from '@mui/material/Grid';
-import useMediaQuery from '@mui/material/useMediaQuery';
-
+import SplitterContainer from '../../components/layout/SplitterContainer';
 import SigninForm from '../../components/forms/SigninForm';
 
 function SigninPage() {
-  const isMobileView = useMediaQuery('(max-width:900px)');
-
   return (
-    <Grid
-      container
-      direction={isMobileView ? 'column' : 'row'}
-      alignItems="center"
-    >
-      <Grid item xs={6}>
+    <SplitterContainer
+      primaryChild={
         <img
           src={require('../../assets/signin.svg').default}
           alt="Signin"
           height="100%"
           width="100%"
         />
-      </Grid>
-      <Grid item xs={6}>
-        <SigninForm />
-      </Grid>
-    </Grid>
+      }
+      secondaryChild={<SigninForm />}
+    />
   );
 }
 
